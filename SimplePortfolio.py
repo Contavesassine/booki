5eea6e6b29884cb56fd02c8fdce0345e46ed596e
Ephrim Bazile <89447659+kevenbazile@users.noreply.github.com>
2025-06-27 12:00:27 -0700

    Update SimplePortfolio.py

--- SimplePortfolio.py
+++ SimplePortfolio.py
@@ -25,7 +25,7 @@ class SimplePortfolio(IStrategy):
     can_short = False
     
     position_adjustment_enable = True
-    max_entry_position_adjustment = 4
+    max_entry_position_adjustment = 8
     
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         """Basic indicators"""
